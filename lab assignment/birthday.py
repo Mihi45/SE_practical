e05751a998b595c4cf85fa06dec1394305ff1bcb
Mihir Manoj Jawale <102919397+Mihi45@users.noreply.github.com>
2023-01-06 12:56:12 +0530

    Update birthday.py

--- lab assignment/birthday.py
+++ lab assignment/birthday.py
@@ -1,91 +1,50 @@
 import datetime
-import imghdr
-import mysql.connector
-import os
-import pywhatkit
 import smtplib
-import get
-from email.message import EmailMessage
 
-Senderemail = get.email
-SenderPassword = get.password
+# Set your email credentials
+EMAIL_ADDRESS = "mihir12242002@gmail.com"
+EMAIL_PASSWORD = "mmxheyujnvikfnuy"
 
-now = str(datetime.date.today())
-#print(now) #format of date = YYYY-MM-DD
-today=now[8:10]+now[5:7]
-print("Todays date "+today)  #format of birthday=DDMM
+# Connect to the email server
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
-#database connection
-mydb = mysql.connector.connect(host="localhost",user="root",password="123",database = "birthday")
-mycursor = mydb.cursor()
+def send_email(recipient_email, subject, body):
+    """Sends an email to the specified recipient."""
+    server.sendmail(EMAIL_ADDRESS, recipient_email, f"Subject: {subject}\n\n{body}")
 
+def send_birthday_greetings(user_id, users_data):
+    """Sends birthday greetings to the specified user via email."""
+    # Fetch the user's birthday and email address from the users_data dictionary
+    birthday = users_data[user_id]["birthday"]
+    email = users_data[user_id]["email"]
 
-#inserting data into database
-def add_data():
-    mycursor1 = mydb.cursor()
-    print("Enter details of person")
-    name = input("Enter name:")
-    birth_date = input("Enter birthdate in format datemonth like 2112 :")
-    number = input("Enter mobile number :")
-    email = input("Enter Email id: ")
-    sql = "INSERT INTO data (name, birth_date,mobile_no,email_id ) VALUES (%s, %s, %s,%s)"
-    person = (name,birth_date,number,email)
-    mycursor1.execute(sql, person)
-    mydb.commit()
-    print("Data added!!")
+    # Check if today is the user's birthday
+    today = datetime.datetime.now()
+    if (today.month, today.day) == (birthday.month, birthday.day):
+        # Send birthday greetings via email
+        subject = "Happy Birthday!"
+        body = f"Happy birthday, {users_data[user_id]['name']}!\n\nWishing you a fantastic day and a great year ahead."
+        send_email(email, subject, body)
 
-#display the persons data from table
-def display_data():
-    display=mydb.cursor()
-    display.execute("select * from data")
-    person_list=display.fetchall()
-    for person in person_list:
-        print(person)
+# Example data for two users
+users_data = {
+    "user1": {
+        "name": "Tommy",
+        "birthday": datetime.datetime(2000, 1, 6),
+        "email": "mihirjawale2412@gmail.com",
+    },
+    "user2": {
+        "name": "Jane",
+        "birthday": datetime.datetime(2006, 4, 1),
+        "email": "aaditi0104@gmail.com",
+    },
+}
 
-#method for sending whatsapp message
-def send_whatsappmessage(data):
-    name =data[0]
-    number='+91'+data[2]
-    img="C:\\Users\\SHRAVANI\\OneDrive\\Documents\\3rdYEAR\\3rd_year\\SE\\birthday_img.jpg"
-    pywhatkit.sendwhats_image(number,img,'Hi '+name+'\nMany many happy returns of the day!',15,True,3)
+# Send birthday greetings to all users
+for user_id in users_data:
+    send_birthday_greetings(user_id, users_data)
 
-#method for sending image on email
-def send_emailimage(data):
-    with open ("birthday_img.jpg",'rb') as m:
-        file_data = m.read()
-        file_type = imghdr.what(m.name)
-        file_name = m.name
-
-    receiveremail=data[3]
-    msg=EmailMessage()
-    msg['Subject']='Sending birthday wishes'
-    msg['From']=Senderemail
-    msg['to']=receiveremail
-    msg.set_content("Hi "+data[0]+"\n Happy Birthday")
-    msg.add_attachment(file_data,maintype='image',subtype=file_type,filename=file_name)
-    #login into senders server
-    server = smtplib.SMTP('smtp.gmail.com',587)
-    server.starttls()
-    server.login(Senderemail,SenderPassword)
-    server.send_message(msg)
-    print("greetings sent !")
-    server.quit()
-
-def send_emailmessage(data):
-    server=smtplib.SMTP('smtp.gmail.com',587)
-    server.starttls()
-    receiveremail=data[3]
-    msg='Hi'+data[0]+" \n many many happy returns of the day!"
-    server.login(Senderemail,SenderPassword)
-    server.sendmail(Senderemail,receiveremail,msg)
-    print("email text sent ")
-    server.quit()
-
-add_data()
-display_data()
-mycursor.execute(f"select * from data where birth_date = {today}")
-li =mycursor.fetchall()
-for i in li:
-    send_whatsappmessage(i)
-    send_emailimage(i)
-    send_emailmessage(i)
+# Disconnect from the email server
+server.quit()
